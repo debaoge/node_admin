ff4bed04f685478187de775329973636e0af14e2
dawang <dawang@xuexi.com>
2025-02-21 15:55:40 +0100

    ProductAdd productList

--- src/views/product-manage/ProductAdd.vue
+++ src/views/product-manage/ProductAdd.vue
@@ -1,5 +1,88 @@
 <template>
     <div>
-        product add 
+        <el-page-header title="产品管理" icon="" content="添加产品" />
+
+        <el-form ref="productFormRef" :model="productForm" :rules="productFormRules" label-width="120px">
+            <el-form-item label="产品名称" prop="title">
+                <el-input v-model="productForm.title" />
+            </el-form-item>
+            <el-form-item label="简要描述" prop="describe">
+                <el-input type="textarea" v-model="productForm.describe" />
+            </el-form-item>
+            <el-form-item label="详细描述" prop="content">
+                <el-input type="textarea" v-model="productForm.content" />
+            </el-form-item>
+                        <el-form-item label="产品图片" prop="cover">
+                <Upload :avatar="productForm.cover" @update-avatar="handleUpdateAvatar" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="handleSubmit">添加产品</el-button>
+            </el-form-item>
+        </el-form>
     </div>
 </template>
+
+<script setup>
+
+import { reactive, ref } from 'vue';
+import Upload from '@/components/upload/Upload.vue';
+import upload from '@/util/upload';
+import { ElMessage } from 'element-plus';
+import { useRouter } from 'vue-router';
+
+const router = useRouter(); 
+const productFormRef = ref()
+const productForm = reactive({
+    title:'',
+    describe:'',
+    content:'',
+    cover:''
+})
+
+const productFormRules = {
+    title: [
+        { required: true, message: '请输入产品名称', trigger: 'blur' },
+        { min: 1, max: 125, message: '长度应为 1 到 125 个字符', trigger: 'blur' },
+    ],
+    describe: [
+        { required: true, message: '请输入产品描述', trigger: 'blur' },
+        { min: 1, max: 125, message: '长度应为 1 到 125 个字符', trigger: 'blur' },
+    ],
+    content: [
+        { required: true, message: '请输入产品详情', trigger: 'blur' },
+        { min: 1, max: 125, message: '长度应为 1 到 1250 个字符', trigger: 'blur' },
+    ],
+};
+
+const handleUpdateAvatar = (newAvatar) => {
+  productForm.cover = newAvatar; 
+};
+
+const handleSubmit = async () => {
+  productFormRef.value.validate(async (valid) => {
+    if (valid) {
+      try {
+        console.log('提交的数据:', productForm); 
+        const res = await upload('/adminapi/product/add', productForm);
+        console.log('服务器返回的数据:', res); 
+
+        if (res.ActionType === 'OK') {
+          ElMessage.success('添加成功');
+          router.push('/mainbox/product-manage/productlist');
+        } else {
+          console.log('ActionType is undefined:', res.data);
+        }
+      } catch (error) {
+        console.error('请求失败:', error); 
+      }
+    } 
+  });
+};
+
+
+</script>
+<style lang="scss" scoped>
+.el-form {
+    margin-top: 40px;
+}
+</style>
--- src/views/product-manage/ProductList.vue
+++ src/views/product-manage/ProductList.vue
@@ -1,5 +1,104 @@
 <template>
-    <div>
-        product list 
-    </div>
+  <div>
+    <el-card>
+      <el-page-header title="产品管理" icon="" content="产品列表" />
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="title" label="产品名称" />
+        <el-table-column prop="describe" label="简要概述" />
+        <el-table-column prop="editTime" label="更新时间">
+          <template #default="scope">
+            {{ formatTime.getTime(scope.row.editTime) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template #default="scope">
+            <el-button size="small" circle :icon="Edit" type="primary" @click="handleEdit(scope.row)"></el-button>
+            <el-popconfirm title="确定要删除吗?" @confirm="handleDelete(scope.row)">
+              <template #reference>
+                <el-button size="small" circle :icon="Delete" type="danger"></el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+
+    <el-dialog v-model="editDialogVisible" title="编辑产品" width="50%">
+      <el-form ref="editFormRef" label-width="120px" :model="editForm">
+        <el-form-item label="标题" prop="title">
+          <el-input v-model="editForm.title" />
+        </el-form-item>
+        <el-form-item label="概要" prop="describe">
+          <el-input type="textarea" v-model="editForm.describe" />
+        </el-form-item>
+        <el-form-item label="详细内容" prop="content">
+          <el-input type="textarea" v-model="editForm.content" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="editDialogVisible = false">取消</el-button>
+          <el-button type="primary" @click="handleEditConfirm">确认</el-button>
+        </div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import formatTime from '@/util/formatTime';
+import axios from 'axios';
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { ElMessage } from 'element-plus';
+
+const tableData = ref([])
+const editFormRef = ref(null)
+const editDialogVisible = ref(false)
+const editForm = ref({
+    title: '',
+    describe: '',
+    content: '',
+    cover:'',
+    editTime: ''
+});
+
+
+onMounted(() => {
+    getTableData()
+})
+
+const getTableData = async () => {
+    const res = await axios.get('/adminapi/product/list')
+    if (res.data.ActionType === 'OK') {
+        tableData.value = res.data.data
+    } else {
+        console.log('读取失败：返回数据格式不正确');
+    }
+}
+
+const handleEdit = async (data) => {
+    editDialogVisible.value = true
+    const res = await axios.get(`adminapi/product/list/${data._id}`);
+    console.log('retrieve from DB: res: ', res.data.data[0]);    
+    editForm.value = res.data.data[0]
+}
+const handleDelete = async (data) => {
+    await axios.delete(`/adminapi/product/list/${data._id}`)
+    getTableData()
+}
+
+const handleEditConfirm = async () => {
+    editDialogVisible.value = false
+    editFormRef.value.validate(async (valid) => {
+        if (valid) {
+            console.log('before update db, editForm value: ', editForm.value);
+            
+            const res = await axios.put(`adminapi/product/list/${editForm.value._id}`, editForm.value);
+            if(res.data.ActionType === 'OK'){
+                ElMessage.success('更新成功')
+            }
+        } 
+    })
+    getTableData(); 
+}
+</script>
